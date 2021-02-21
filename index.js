83010b799b0942d7ba442b89be5b6566b8b046f5
Christoph Müller <mueller@olyro.de>
2021-02-21 16:36:55 +0100

    render random tile + random direction

--- index.js
+++ index.js
@@ -7,17 +7,24 @@
       i.src = url;
     });
   };
+  var getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  };
   var generateBoard = async (height, width) => {
-    let img = await loadImage("./grass.png");
+    const images = [await loadImage("./grass.png"), await loadImage("./flowers.png"), await loadImage("./dirt.png")];
     const scale = 1;
-    const s = img.width / 8 - 2;
+    const defaultHeight = images[0].height;
     const canvas = document.getElementById("main-canvas");
     if (canvas) {
       const ctx = canvas.getContext("2d");
       if (ctx) {
         for (let x = 0; x < width; x++) {
           for (let y = 0; y < height; y++) {
-            ctx.drawImage(img, 2 + img.width / 4, 2, img.width / 4 - 4, img.height - 4, s * (width - x + y), (x + y) * (s / 2), (img.width / 4 - 4) * scale, (img.height - 4) * scale);
+            const choose = getRandomInt(images.length);
+            const direction = getRandomInt(4);
+            const img = images[choose];
+            const s = img.width / 8 - 4;
+            ctx.drawImage(img, 2 + img.width / 4 * direction, 2, img.width / 4 - 4, img.height - 4, s * (width - x + y), (x + y) * (s / 2) + (defaultHeight - img.height), (img.width / 4 - 4) * scale, (img.height - 4) * scale);
           }
         }
       }
--- index.ts
+++ index.ts
@@ -1,25 +1,32 @@
-let loadImage = (url: string): Promise<HTMLImageElement> => {
+const loadImage = (url: string): Promise<HTMLImageElement> => {
     return new Promise(r => { let i = new Image(); i.onload = (() => r(i)); i.src = url; });
 }
 
+const getRandomInt = (max: number) => {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
 const generateBoard = async (height: number, width: number) => {
-    let img = await loadImage("./grass.png");
+    const images = [await loadImage("./grass.png"), await loadImage("./flowers.png"), await loadImage("./dirt.png")];
     const scale = 1.0;
-    const s = (img.width / 8) - 2;
+    const defaultHeight = images[0].height;
     const canvas = document.getElementById('main-canvas');
     if (canvas) {
         const ctx = (canvas as HTMLCanvasElement).getContext('2d');
         if (ctx) {
             for (let x = 0; x < width; x++) {
                 for (let y = 0; y < height; y++) {
+                    const choose = getRandomInt(images.length)
+                    const direction = getRandomInt(4)
+                    const img = images[choose];
+                    const s = (img.width / 8) - 4;
                     ctx.drawImage(
                         img,
-                        2 + img.width / 4,
+                        2 + (img.width / 4) * direction,
                         2,
                         img.width / 4 - 4,
                         img.height - 4,
-                        s * (width - x + y), (x + y) * (s / 2),
+                        s * (width - x + y), (x + y) * (s / 2) + (defaultHeight - img.height),
                         (img.width / 4 - 4) * scale,
                         (img.height - 4) * scale
                     );
