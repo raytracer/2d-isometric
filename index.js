328eb30494795be823d298c03ccc083a0411c656
Christoph Müller <mueller@olyro.de>
2021-02-28 00:09:50 +0100

    make overlay

--- index.js
+++ index.js
@@ -13,35 +13,83 @@
   var scale = 1;
   var offsetX = 0;
   var offsetY = 0;
-  var pressed = false;
   var divider = window.navigator.platform.toLowerCase().indexOf("mac") === -1 ? window.devicePixelRatio : 1;
+  var canvas = document.getElementById("main-canvas");
+  if (canvas) {
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    canvas.width = vw;
+    canvas.height = vh;
+  }
   var cursorX = 0;
   var cursorY = 0;
-  document.onmousedown = () => pressed = true;
-  document.onmouseup = () => pressed = false;
-  document.onmousemove = (event) => {
-    if (pressed) {
-      offsetX += event.movementX / divider / scale;
-      offsetY += event.movementY / divider / scale;
-    }
-    ;
-    cursorX = event.offsetX;
-    cursorY = event.offsetY;
-  };
-  document.onkeypress = (event) => {
-    switch (event.key) {
-      case "w":
-        scale = Math.min(scale + 0.25, 1.5);
-        break;
-      case "s":
-        scale = Math.max(0.5, scale - 0.25);
-        break;
-    }
-  };
+  var moveX = 0;
+  var moveY = 0;
   var start = async () => {
     const images = [await loadImage("./grass.png"), await loadImage("./flowers.png"), await loadImage("./dirt.png")];
     const house = await loadImage("./house.png");
+    const s = house.width / 8 - 4;
     const defaultHeight = images[0].height;
+    const getNextCursorAdjacentTile = () => {
+      let ox = (-width * s - offsetX - s) / 2;
+      let oy = (s + offsetY) / 2;
+      let x = Math.floor((cursorY / (2 * scale) - oy) / (s / 2) + (-cursorX / (scale * 2) - ox) / s);
+      let y = Math.floor((cursorY / (2 * scale) - oy) / (s / 2) - (-cursorX / (scale * 2) - ox) / s);
+      x = Math.min(width - 1, Math.max(0, x));
+      y = Math.min(height - 1, Math.max(0, y));
+      return board.filter((d) => d.x === x && d.y === y)[0];
+    };
+    const buildHouse = () => {
+      const tile = getNextCursorAdjacentTile();
+      const x = tile.x;
+      const y = tile.y;
+      board.splice(board.indexOf(tile), 1);
+      board = board.filter((d) => d.x !== x || d.y !== y);
+      board.push({
+        x,
+        y,
+        z: 0,
+        draw: (ctx) => {
+          ctx.drawImage(house, 2 + house.width / 4 * 3, 2, house.width / 4 - 4, house.height - 4, (s * (width - x + y) + offsetX) * scale, ((x + y) * (s / 2) + (defaultHeight - house.height) + offsetY) * scale, (house.width / 4 - 4) * scale, (house.height - 4) * scale);
+        }
+      });
+    };
+    document.onmouseup = () => {
+      buildHouse();
+    };
+    document.onmousemove = (event) => {
+      moveX = 0;
+      moveY = 0;
+      const speed = 30;
+      const edge = 50;
+      if (canvas) {
+        if (event.screenX < edge) {
+          moveX = speed;
+        }
+        if (event.screenY < edge * 2) {
+          console.log("hi");
+          moveY = speed;
+        }
+        if (event.screenX > canvas.width - edge) {
+          moveX = -speed;
+        }
+        if (event.screenY > canvas.height - edge) {
+          moveY = -speed;
+        }
+      }
+      cursorX = event.offsetX;
+      cursorY = event.offsetY;
+    };
+    document.onkeypress = (event) => {
+      switch (event.key) {
+        case "w":
+          scale = Math.min(scale + 0.25, 1.5);
+          break;
+        case "s":
+          scale = Math.max(0.5, scale - 0.25);
+          break;
+      }
+    };
     const generateBoard = (height2, width2) => {
       const result = [];
       for (let x = 0; x < width2; x++) {
@@ -54,8 +102,8 @@
             y,
             z: 0,
             draw: (ctx) => {
-              const s = img.width / 8 - 4;
-              ctx.drawImage(img, 2 + img.width / 4 * direction, 2, img.width / 4 - 4, img.height - 4, (s * (width2 - x + y) + offsetX) * scale, ((x + y) * (s / 2) + (defaultHeight - img.height) + offsetY) * scale, (img.width / 4 - 4) * scale, (img.height - 4) * scale);
+              const s2 = img.width / 8 - 4;
+              ctx.drawImage(img, 2 + img.width / 4 * direction, 2, img.width / 4 - 4, img.height - 4, (s2 * (width2 - x + y) + offsetX) * scale, ((x + y) * (s2 / 2) + (defaultHeight - img.height) + offsetY) * scale, (img.width / 4 - 4) * scale, (img.height - 4) * scale);
             }
           });
         }
@@ -63,13 +111,9 @@
       return result;
     };
     const getHouseOverlay = (width2, height2) => {
-      const s = house.width / 8 - 4;
-      let ox = (-width2 * s - offsetX - s) / 2;
-      let oy = (s + offsetY) / 2;
-      let x = Math.floor((cursorY / (2 * scale) - oy) / (s / 2) + (-cursorX / (scale * 2) - ox) / s);
-      let y = Math.floor((cursorY / (2 * scale) - oy) / (s / 2) - (-cursorX / (scale * 2) - ox) / s);
-      x = Math.min(width2 - 1, Math.max(0, x));
-      y = Math.min(height2 - 1, Math.max(0, y));
+      const tile = getNextCursorAdjacentTile();
+      const x = tile.x;
+      const y = tile.y;
       return {
         x,
         y,
@@ -83,9 +127,10 @@
     };
     const width = 12;
     const height = 12;
-    const board = await generateBoard(height, width);
+    let board = await generateBoard(height, width);
     const drawBoard = () => {
-      const canvas = document.getElementById("main-canvas");
+      offsetX += moveX / divider / scale;
+      offsetY += moveY / divider / scale;
       if (canvas) {
         const ctx = canvas.getContext("2d");
         if (ctx) {
--- index.ts
+++ index.ts
@@ -16,39 +16,110 @@ interface Drawable {
 let scale = 1.0;
 let offsetX = 0;
 let offsetY = 0;
-let pressed = false;
 const divider = window.navigator.platform.toLowerCase().indexOf("mac") === -1 ? window.devicePixelRatio : 1;
+const canvas = document.getElementById('main-canvas');
+
+if (canvas) {
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    (canvas as HTMLCanvasElement).width = vw;
+    (canvas as HTMLCanvasElement).height = vh;
+}
 
 let cursorX = 0;
 let cursorY = 0;
 
-document.onmousedown = () => pressed = true;
-document.onmouseup = () => pressed = false;
-document.onmousemove = (event) => {
-    if (pressed) {
-        offsetX += event.movementX / divider / scale;
-        offsetY += event.movementY / divider / scale;
-    };
-
-    cursorX = event.offsetX;
-    cursorY = event.offsetY;
-}
-
-document.onkeypress = (event) => {
-    switch (event.key) {
-        case "w":
-            scale = Math.min(scale + 0.25, 1.5);
-            break;
-        case "s":
-            scale = Math.max(0.5, scale - 0.25);
-            break;
-    }
-};
+let moveX = 0;
+let moveY = 0;
 
 const start = async () => {
     const images = [await loadImage("./grass.png"), await loadImage("./flowers.png"), await loadImage("./dirt.png")];
     const house = await loadImage("./house.png");
+    const s = (house.width / 8) - 4;
     const defaultHeight = images[0].height;
+
+    const getNextCursorAdjacentTile = (): Drawable => {
+        let ox = (-width * s - offsetX - s) / 2;
+        let oy = (s + offsetY) / 2;
+        let x = Math.floor(((cursorY / (2 * scale) - oy) / (s / 2)) + ((-cursorX / (scale * 2) - ox) / s));
+        let y = Math.floor(((cursorY / (2 * scale) - oy) / (s / 2)) - ((-cursorX / (scale * 2) - ox) / s));
+
+        x = Math.min(width - 1, Math.max(0, x));
+        y = Math.min(height - 1, Math.max(0, y));
+
+        return board.filter(d => d.x === x && d.y === y)[0];
+    };
+
+    const buildHouse = () => {
+        const tile = getNextCursorAdjacentTile();
+        const x = tile.x;
+        const y = tile.y;
+        board.splice(board.indexOf(tile), 1);
+
+        board = board.filter(d => d.x !== x || d.y !== y);
+        board.push({
+            x: x,
+            y: y,
+            z: 0,
+            draw: (ctx: CanvasRenderingContext2D) => {
+                ctx.drawImage(
+                    house,
+                    2 + (house.width / 4) * 3,
+                    2,
+                    house.width / 4 - 4,
+                    house.height - 4,
+                    (s * (width - x + y) + offsetX) * scale, ((x + y) * (s / 2) + (defaultHeight - house.height) + offsetY) * scale,
+                    (house.width / 4 - 4) * scale,
+                    (house.height - 4) * scale
+                );
+            }
+        });
+    };
+
+    document.onmouseup = () => {
+        buildHouse();
+    };
+
+    document.onmousemove = (event) => {
+        moveX = 0;
+        moveY = 0;
+
+        const speed = 30;
+        const edge = 50;
+
+        if (canvas) {
+            if (event.screenX < edge) {
+                moveX = speed;
+            }
+
+            if (event.screenY < edge * 2) {
+                console.log("hi");
+                moveY = speed;
+            }
+
+            if (event.screenX > (canvas as HTMLCanvasElement).width - edge) {
+                moveX = -speed;
+            }
+
+            if (event.screenY > (canvas as HTMLCanvasElement).height - edge) {
+                moveY = -speed;
+            }
+        }
+
+        cursorX = event.offsetX;
+        cursorY = event.offsetY;
+    }
+
+    document.onkeypress = (event) => {
+        switch (event.key) {
+            case "w":
+                scale = Math.min(scale + 0.25, 1.5);
+                break;
+            case "s":
+                scale = Math.max(0.5, scale - 0.25);
+                break;
+        }
+    };
 
     const generateBoard = (height: number, width: number) => {
         const result: Array<Drawable> = [];
@@ -80,14 +151,9 @@ const start = async () => {
     }
 
     const getHouseOverlay = (width: number, height: number): Drawable => {
-        const s = (house.width / 8) - 4;
-        let ox = (-width * s - offsetX - s) / 2;
-        let oy = (s + offsetY) / 2;
-        let x = Math.floor(((cursorY / (2 * scale) - oy) / (s / 2)) + ((-cursorX / (scale * 2) - ox) / s));
-        let y = Math.floor(((cursorY / (2 * scale) - oy) / (s / 2)) - ((-cursorX / (scale * 2) - ox) / s));
-
-        x = Math.min(width - 1, Math.max(0, x));
-        y = Math.min(height - 1, Math.max(0, y));
+        const tile = getNextCursorAdjacentTile();
+        const x = tile.x;
+        const y = tile.y;
 
         return {
             x: x,
@@ -110,13 +176,13 @@ const start = async () => {
         };
     }
 
-
     const width = 12;
     const height = 12;
-    const board = await generateBoard(height, width);
+    let board = await generateBoard(height, width);
 
     const drawBoard = () => {
-        const canvas = document.getElementById('main-canvas');
+        offsetX += moveX / divider / scale;
+        offsetY += moveY / divider / scale;
 
         if (canvas) {
             const ctx = (canvas as HTMLCanvasElement).getContext('2d');
